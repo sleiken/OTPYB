86d498751437d29ff6e88461edcae773da9b1e92
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-12 20:32:08 -0700

    adds reg form to login div

--- public/js/application.js
+++ public/js/application.js
@@ -55,44 +55,7 @@ var setupHeaderForm = function( response ) {
    targetParentListener.append(response);
 };
 
-// var generateRandomColor = function () {
-//    var num = Math.floor((Math.random() * 4) + 0);
-//
-//    switch ( num ) {
-//       case 0:
-//          return "green"
-//          break;
-//       case 1:
-//          return "red"
-//          break;
-//       case 2:
-//          return "pink"
-//          break;
-//       case 3:
-//          return "orange"
-//          break;
-//       case 4:
-//          return "blue"
-//          break;
-//       default:
-//          return "blue"
-//    }
-// }
-//
-// var doSomeCrazyStuff = function() {
-//    $( "div" ).on( "mouseover", "h1", function( event ) {
-//       event.preventDefault();
-//       var targetElement = $( this );
-//       targetElement.css('color', generateRandomColor());
-//    });
-// };
-//
-// var setupHeaderForm = function( response ) {
-//    var targetParentListener = $ ( "#header_login_register_div" );
-//    targetParentListener.empty();
-//    targetParentListener.append(response);
-// };
-//
+
 // var headerLinkListener = function(link_class_name) {
 //    $( "#header_container" ).on( "click", link_class_name, function( event ) {
 //       event.preventDefault();
