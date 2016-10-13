3207f1549acc66530738509a4492e47c2d6d17bd
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-12 17:01:30 -0700

    fixes seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,6 @@ User.all.destroy_all
 Visit.all.destroy_all
 Place.all.destroy_all
 
-username = Faker::Internet.user_name
-email = Faker::Internet.email
-city = Faker::Address.city
-country = Faker::Address.country
-state = Faker::Address.state
-lat = Faker::Address.latitude
-lng = Faker::Address.longitude
-
 users = []
 5.times do |i|
   users << User.create(username: Faker::Internet.user_name,
@@ -21,7 +13,7 @@ places = []
 10.times do |i|
   places << Place.create(city:    Faker::Address.city,
                          country: Faker::Address.country,
-                         state:   state = Faker::Address.state,
+                         state:   Faker::Address.state,
                          lat:     Faker::Address.latitude,
                          lng:     Faker::Address.longitude)
 end
