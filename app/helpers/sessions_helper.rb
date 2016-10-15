6a6612d71093a75f0ac0821114aae5c509b7928b
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-14 23:29:42 -0700

    adds username to navbar

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 helpers do
-
    def current_user
       @current_user ||= User.find(session[:id]) if session[:id]
    end
@@ -7,5 +6,4 @@ helpers do
    def logged_in?
       current_user
    end
-
 end
