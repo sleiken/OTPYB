8eb31e50120d1c0501d248962d0d2564233870ca
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-15 17:47:28 -0700

    adds places to db for users

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,14 @@
-get '/' do
+get "/" do
 	@api_key = ENV['YOUR_API_KEY']
 	@places = User.find(session[:id]).places if logged_in?
+	p @places
 	erb :index, layout: !request.xhr?
+end
+
+post "/places" do
+	p params[:placeId][0...-1]
+
+	place = Place.create!(placeId: params[:placeId][0...-1])
+	Visit.create!(user_id: current_user.id, place_id: place.id)
+	redirect "/"
 end
