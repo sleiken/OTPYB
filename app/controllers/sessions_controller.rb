dab1d4ba389a4f7f02b67cdd83e791bee95814dd
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-21 11:56:02 -0700

    fixes some spacing issues and adds media query for mobile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
-#NEW
 get '/sessions/login/?' do
-   erb :'_login.html', layout: !request.xhr?
+  erb :'_login.html', layout: !request.xhr?
 end
 
-#CREATE
 post '/sessions/login/?' do
   @user = User.find_by(email: params[:user][:email])
   if @user && @user.password == params[:user][:password]
@@ -14,8 +12,7 @@ post '/sessions/login/?' do
   end
 end
 
-#GET
 get '/sessions/logout/?' do
-   session[:id] = nil
-   redirect "/"
+  session[:id] = nil
+  redirect "/"
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,33 @@
 get '/users/?' do
-   redirect "/"
+  redirect "/"
 end
-
-#NEW (GET)
 
 get '/users/new/?' do
-   erb :'_new.html', layout: !request.xhr?
+  erb :'_new.html', layout: !request.xhr?
 end
-
-#SHOW (GET)
 
 get '/users/:id/?' do
-
-   @user = User.find(params[:id])
-
-   if current_user == @user
-      redirect "/users/#{@user.id}"
-   else
-      redirect "/"
-   end
-
+  @user = User.find(params[:id])
+  if current_user == @user
+    redirect "/users/#{@user.id}"
+  else
+    redirect "/"
+  end
 end
 
-#CREATE (POST)
 
 post '/users/?' do
-
-   if params[:password_confirmation] == params[:user][:password]
-
-      @user = User.new(params[:user])
-
-      if @user.save
-         session[:id] = @user.id
-         erb :'_header_links.html', layout: !request.xhr?
-      else
-         @errors = @user.errors.full_messages
-         erb :'_new.html', layout: !request.xhr?
-      end
-   else
-      @errors = "Passwords do not match!"
-      erb :'_new.html', layout: !request.xhr?
-   end
-
+  if params[:password_confirmation] == params[:user][:password]
+    @user = User.new(params[:user])
+    if @user.save
+       session[:id] = @user.id
+       erb :'_header_links.html', layout: !request.xhr?
+    else
+       @errors = @user.errors.full_messages
+       erb :'_new.html', layout: !request.xhr?
+    end
+  else
+    @errors = "Passwords do not match!"
+    erb :'_new.html', layout: !request.xhr?
+  end
 end
-
-#EDIT (GET)
-
-#UPDATE (PUT)
-
-#DELETE (DELETE)
